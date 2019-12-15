dc971c19430ed7fbf92dc430ba584cea42afc2d5
Sune Simonsen <sune@we-knowhow.dk>
2019-12-15 21:45:10 +0100

    Fixed linting problem

--- packages/core/src/withGroup.js
+++ packages/core/src/withGroup.js
@@ -2,7 +2,6 @@ const map = require("./map");
 const pipeline = require("./pipeline");
 const takeAll = require("./takeAll");
 const emitItems = require("./emitItems");
-const tap = require("./tap");
 
 const withGroup = (...steps) =>
   map(async group => ({
