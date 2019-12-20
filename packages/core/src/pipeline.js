c20c4db907351122e94469ef5db46c9d08834053
Sune Simonsen <sune@we-knowhow.dk>
2019-12-20 22:32:38 +0100

    Support returning a promise from the setup part in a step

--- packages/core/src/pipeline.js
+++ packages/core/src/pipeline.js
@@ -1,16 +1,36 @@
-const { chan } = require("medium");
+const { chan, go, take, put, close, CLOSED } = require("medium");
 
 const pipeline = (...steps) => (input, errors) => {
-  let channel = input || chan();
+  const output = chan();
 
-  steps.forEach(stepOrChannel => {
-    channel =
-      typeof stepOrChannel === "function"
-        ? stepOrChannel(channel, errors)
-        : stepOrChannel;
+  go(async () => {
+    let channel = input || chan();
+
+    try {
+      for (let stepOrChannel of steps) {
+        if (!stepOrChannel.buffer && stepOrChannel.then) {
+          stepOrChannel = await stepOrChannel;
+        }
+
+        channel =
+          typeof stepOrChannel === "function"
+            ? stepOrChannel(channel, errors)
+            : stepOrChannel;
+      }
+
+      while (true) {
+        const value = await take(channel);
+        if (value === CLOSED) break;
+        await put(output, value);
+      }
+    } catch (err) {
+      await put(errors, err);
+    } finally {
+      close(output);
+    }
   });
 
-  return channel;
+  return output;
 };
 
 module.exports = pipeline;
