b45a14f80211fd950ab732d6b4099bb33e218561
Sune Simonsen <sune@we-knowhow.dk>
2019-12-29 23:18:46 +0100

    Refactor transform

--- packages/core/src/transform.js
+++ packages/core/src/transform.js
@@ -4,15 +4,19 @@ const emitItems = require("./emitItems");
 const takeAll = require("./takeAll");
 
 const executeTransformation = async (transformation, value) => {
-  if (typeof transformation === "undefined" || typeof value === "undefined") {
-    return value;
-  }
-
   if (!value) {
     return value;
   }
 
+  if (typeof transformation === "undefined") {
+    return value;
+  }
+
   if (transformation.type === "step") {
+    if (typeof value === "undefined") {
+      return value;
+    }
+
     const result = await takeAll(pipeline(emitItems(value), transformation));
 
     if (result.length !== 1) {
@@ -20,28 +24,44 @@ const executeTransformation = async (transformation, value) => {
     }
 
     return result[0];
-  } else if (Array.isArray(value)) {
-    return value.map((item, i) =>
-      executeTransformation(transformation[i], item)
-    );
-  } else if (typeof value === "object" && value.constructor === Object) {
-    const keys = Object.keys(value);
-    const transformedObject = keys.reduce((result, key) => {
-      result[key] = executeTransformation(transformation[key], value[key]);
-      return result;
-    }, {});
+  }
 
-    for (let key of keys) {
-      transformedObject[key] = await transformedObject[key];
-    }
+  if (Array.isArray(transformation)) {
+    return Promise.all(
+      transformation.map((_, i) =>
+        executeTransformation(transformation[i], value[i])
+      )
+    );
+  }
+
+  if (
+    typeof transformation === "object" &&
+    transformation.constructor === Object
+  ) {
+    const transformedObject = { ...value };
+
+    const entries = await Promise.all(
+      Object.keys(transformation).map(async key => [
+        key,
+        await executeTransformation(transformation[key], value[key])
+      ])
+    );
+
+    entries.forEach(([key, value]) => {
+      transformedObject[key] = value;
+    });
 
     return transformedObject;
-  } else {
-    return value;
   }
+
+  return value;
 };
 
 const transform = transformation =>
-  map(value => executeTransformation(transformation, value));
+  map(value =>
+    value && typeof value === "object"
+      ? executeTransformation(transformation, value)
+      : value
+  );
 
 module.exports = transform;
