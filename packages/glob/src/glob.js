7828746efaf2b5759bbb1cbc233718bd72ef3c10
Sune Simonsen <sune@we-knowhow.dk>
2020-02-10 22:04:46 +0100

    Rewrite the glob step to allow all glob options

--- packages/glob/src/glob.js
+++ packages/glob/src/glob.js
@@ -1,6 +1,24 @@
-const { emitItems, pipeline } = require("@transformation/core");
-const globEach = require("./globEach");
+const globby = require("globby");
+const path = require("path");
+const { step } = require("@transformation/core");
 
-const glob = (options = {}) => pipeline(emitItems(options), globEach());
+const glob = (options = {}) =>
+  step(async ({ put, CLOSED }) => {
+    options =
+      typeof options === "string" || Array.isArray(options)
+        ? { pattern: options }
+        : options;
+
+    for await (const pathName of globby.stream(options.pattern, {
+      ...options,
+      absolute: false
+    })) {
+      if (options.absolute) {
+        await put(path.resolve(options.cwd, pathName));
+      } else {
+        await put(pathName);
+      }
+    }
+  });
 
 module.exports = glob;
