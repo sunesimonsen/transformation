8e70fec9ba5eb2cde17f229191ffaeb433c65cab
Sune Simonsen <sune@we-knowhow.dk>
2020-06-01 22:15:27 +0200

    Ups committed debug code :-S

--- packages/core/src/memorize.js
+++ packages/core/src/memorize.js
@@ -3,7 +3,6 @@ const chose = require("./chose");
 const map = require("./map");
 const transform = require("./transform");
 const forEach = require("./forEach");
-const tap = require("./tap");
 const QuickLRU = require("quick-lru");
 
 const memorize = (step, { maxSize = 1000, key = v => v } = {}) => {
@@ -12,22 +11,16 @@ const memorize = (step, { maxSize = 1000, key = v => v } = {}) => {
   const lru = new QuickLRU({ maxSize });
 
   return pipeline(
-    tap(v => `check: ${keySelector(v)}`),
     chose(value => (lru.has(keySelector(value)) ? "cached" : "notCached"), {
-      cached: pipeline(
-        tap(v => `cached: ${keySelector(v)} ${v}`),
-        map(value => lru.get(keySelector(value)))
-      ),
+      cached: map(value => lru.get(keySelector(value))),
       notCached: pipeline(
         map(value => ({ key: keySelector(value), output: value })),
         transform({
           output: step
         }),
         forEach(({ key, output }) => {
-          console.log(key, output);
           lru.set(key, output);
         }),
-        tap(v => `cached: ${v.key}`),
         map(({ output }) => output)
       )
     })
