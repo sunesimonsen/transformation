3d43472f7565542866657c5b8845fda475269f3e
Sune Simonsen <sune@we-knowhow.dk>
2020-06-21 15:59:25 +0200

    Add the item index to filter, forEach, map and reduce

--- packages/core/src/reduce.js
+++ packages/core/src/reduce.js
@@ -2,12 +2,13 @@ const step = require("./step");
 
 const reduce = (accumulator, initialValue) =>
   step(async ({ take, put, CLOSED }) => {
+    let i = 0;
     let accumulation = initialValue;
 
     while (true) {
       const value = await take();
       if (value === CLOSED) break;
-      accumulation = await accumulator(accumulation, value);
+      accumulation = await accumulator(accumulation, value, i++);
     }
 
     await put(accumulation);
