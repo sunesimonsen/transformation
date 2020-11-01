e17c36cce4f58d1abac4926fa35324cbfed2a31c
Sune Simonsen <sune@we-knowhow.dk>
2020-11-01 09:40:14 +0100

    Make flatMap independent of pipeline

--- packages/core/src/flatMap.js
+++ packages/core/src/flatMap.js
@@ -1,7 +1,28 @@
-const pipeline = require("./pipeline");
-const map = require("./map");
-const splitIterable = require("./splitIterable");
+const step = require("./step");
 
-const flatMap = (...args) => pipeline(map(...args), splitIterable());
+const flatMap = mapper =>
+  step(async ({ take, put, CLOSED }) => {
+    let i = 0;
+    while (true) {
+      const value = await take();
+      if (value === CLOSED) break;
+
+      const result = await mapper(value, i++);
+
+      const isIterable =
+        result &&
+        typeof result !== "string" &&
+        (typeof result[Symbol.iterator] === "function" ||
+          typeof result[Symbol.asyncIterator] === "function");
+
+      if (isIterable) {
+        for await (let item of result) {
+          await put(item);
+        }
+      } else {
+        await put(result);
+      }
+    }
+  });
 
 module.exports = flatMap;
