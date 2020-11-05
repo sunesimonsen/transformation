2a4870c6bbb10e9ecd1d7c8ff604c4815a3e589e
Sune Simonsen <sune@we-knowhow.dk>
2020-11-05 22:59:41 +0100

    Allow steps to be returned from the map step

--- packages/core/src/map.js
+++ packages/core/src/map.js
@@ -1,13 +1,42 @@
-const step = require("./step");
+const { go, close, CLOSED, chan, put, take } = require("medium");
+const channelStep = require("./channelStep");
 
 const map = mapper =>
-  step(async ({ take, put, CLOSED }) => {
-    let i = 0;
-    while (true) {
-      const value = await take();
-      if (value === CLOSED) break;
-      await put(await mapper(value, i++));
-    }
+  channelStep((input, errors) => {
+    const output = chan();
+
+    go(async () => {
+      let i = 0;
+      while (true) {
+        const value = await take(input);
+        if (value === CLOSED) break;
+
+        try {
+          const mappedValue = await mapper(value, i++);
+
+          if (
+            mappedValue &&
+            mappedValue.type === "step" &&
+            typeof mappedValue.body === "function"
+          ) {
+            const valueOutput = mappedValue.body(chan());
+            while (true) {
+              const item = await take(valueOutput);
+              if (item === CLOSED) break;
+              await put(output, item);
+            }
+          } else {
+            await put(output, mappedValue);
+          }
+        } catch (e) {
+          put(errors, e);
+        }
+      }
+
+      close(output);
+    });
+
+    return output;
   });
 
 module.exports = map;
--- packages/core/src/map.spec.js
+++ packages/core/src/map.spec.js
@@ -3,6 +3,8 @@ const expect = require("unexpected")
   .use(require("unexpected-steps"));
 const emitItems = require("./emitItems");
 const pipeline = require("./pipeline");
+const emitRange = require("./emitRange");
+const toArray = require("./toArray");
 const map = require("./map");
 
 describe("map", () => {
@@ -15,5 +17,29 @@ describe("map", () => {
       "to yield items",
       [0, 1, 4, 9, 16, 25]
     );
+  });
+
+  it("provides the index of the current item to the mapper function", async () => {
+    await expect(
+      pipeline(
+        emitItems("zero", "one", "two", "three"),
+        map((n, i) => `${i}: ${n}`)
+      ),
+      "to yield items",
+      ["0: zero", "1: one", "2: two", "3: three"]
+    );
+  });
+
+  describe("when the mapper returns a step", () => {
+    it("emits all of the values generated from the step", async () => {
+      await expect(
+        pipeline(
+          emitItems(0, 1, 2, 3, 4),
+          map(n => pipeline(emitRange(n), toArray()))
+        ),
+        "to yield items",
+        [[], [0], [0, 1], [0, 1, 2], [0, 1, 2, 3]]
+      );
+    });
   });
 });
